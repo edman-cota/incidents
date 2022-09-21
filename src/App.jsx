e1dfde43a82a0b91a91346982746af0677688c59
edmancota <edmancota2017@gmail.com>
2022-09-21 11:51:40 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = () => (
+  <div className="w-72 bg-slate-800 h-full">HOla mundo</div>
+);
+
+export default Sidebar;

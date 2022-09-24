7e6acc084b9f7a7116fe56dd2d6564e635b630fb
edmancota <edmancota2017@gmail.com>
2022-09-24 14:01:13 -0600

    Remove the clear button

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -45,14 +45,6 @@ const Main = () => {
               value={incident}
             />
           </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Clear
-            </button>
-          </div>
         </form>
       </div>
     </div>
